5636f2c07571de4f48cb1238f65158a084844fb0
jelle sluyts <jellesluyts@jelles-MacBook-Pro.local>
2021-03-03 21:54:07 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>FIRMA 83</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          FIRMA 83
+        </h1>
+        <p>coming soon!</p>
+      </main>
+    </div>
+  )
+}
