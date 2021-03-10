434f357ef7edac034a304c693f524d8d177a5a0d
jelle sluyts <jellesluyts@jelles-MacBook-Pro.local>
2021-03-10 20:29:23 +0100

    added mail

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { FiMail } from "react-icons/fi";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
           FIRMA 83
         </h1>
         <p>coming soon!</p>
+        <a href="mailto:info@jellesluyts.be"><FiMail /></a>
       </main>
     </div>
   )
